89168d5edac643f2badb19040d64148c188ec800
ismael <ismael.ribeiro@ubook.com>
2021-10-22 01:54:33 -0300

    button alert

---
+++ Button-action/Button_actionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Button_actionApp.swift
+//  Button-action
+//
+//  Created by Ismael on 22/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Button_actionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
